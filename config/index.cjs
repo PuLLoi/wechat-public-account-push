048bb8dde57ac59143ce64cf3032961026f2e354
PuLLoi <103492505+PuLLoi@users.noreply.github.com>
2022-11-07 19:11:00 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ogIzm6FuTXKhk4J3TbWwKk7tSB80',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'Ob4bVlM9U9lMLvmAprxk46_BeBpPfKjzwJpXsxChLEQ',
+      useTemplateId: 'UF5L0HPxpa6G8qxxGXtk5QceCK8Jvheg487zCqRPpjU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-01',
       festivals: [
